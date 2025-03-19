000e87eedbb0a16cb9656cbdc69a9c3d90834e49
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-03-19 22:17:31 +0300

    lab done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form"); // select the form element
+  let tasklist = document.querySelector("#tasks"); // select the ul element
+
+  form.addEventListener("submit", function (event) {
+    // add an event listener to the form
+    event.preventDefault(); // prevent the default action of the form from happening
+
+    let task = document.createElement("li"); // create a new list HTML element (li)
+    let input = document.getElementById("new-task-description"); //
+    task.textContent = input.value;
+    // task.innerText = event.target[0].value; // Add text directly to <li>
+
+    let deleteButton = document.createElement("button"); // create a new button element
+    deleteButton.innerText = "X"; // set the text of the button to "X"
+    deleteButton.addEventListener("click", () => {
+      // add an event listener to the button
+      task.remove(); // Remove task on button click
+      console.log("deleted");
+    });
+
+    task.appendChild(deleteButton); // append the button to the div
+    tasklist.appendChild(task); // display the list item on the page under the ul
+
+    event.target.reset(); // clear the input field after the task is submitted
+  });
 });
